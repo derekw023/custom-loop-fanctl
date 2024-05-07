107177c4084767161d0dd1ff398826e38cbfba76
Dex Witt <dex@witt.me>
2024-05-07 00:34:15 -0400

    Initial scaffold for native plugin

---
+++ fc-plugin/DexControllerSensor/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System.IO.Ports;
+using FanControl.Plugins;
+
+namespace FanControl.DexControllerSensor
+{
+    public class DexPlugin : IPlugin2
+    {
+        public string Name => "DexFan Controller";
+
+        private SerialPort local_port;
+
+        public void Close()
+        {
+            local_port.Close();
+        }
+
+        public void Initialize()
+        {
+            local_port = new SerialPort
+            {
+                PortName = "COM6"
+            };
+
+        }
+
+        public void Load(IPluginSensorsContainer _container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
